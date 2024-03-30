ad6ba2d8d7ed4af1910b0c579c2d0357199f1a0c
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2024-03-30 16:19:21 +0800

    added Cortex

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,66 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Authenticated, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+import { dataProvider, liveProvider, authProvider } from "./providers";
 import routerBindings, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
+    CatchAllNavigate,
+    DocumentTitleHandler,
+    UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
-import { createClient } from "graphql-ws";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { authProvider } from "./authProvider";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-
-const API_URL = "https://api.nestjs-query.refine.dev/graphql";
-const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
-
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
-
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import { Home, ForgotPassword, Login, Register } from "./pages";
+import Layout from "./components/layout";
 function App() {
-  return (
-    <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <AntdApp>
-            <DevtoolsProvider>
-              <Refine
-                dataProvider={dataProvider(gqlClient)}
-                liveProvider={liveProvider(wsClient)}
-                notificationProvider={useNotificationProvider}
-                routerProvider={routerBindings}
-                authProvider={authProvider}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  useNewQueryKeys: true,
-                  projectId: "ZmE05r-dxIvNx-6A4YGY",
-                  liveMode: "auto",
-                }}
-              >
-                <Routes>
-                  <Route index element={<WelcomePage />} />
-                </Routes>
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-              </Refine>
-              <DevtoolsPanel />
-            </DevtoolsProvider>
-          </AntdApp>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <RefineKbarProvider>
+                <AntdApp>
+                    <DevtoolsProvider>
+                        <Refine
+                            dataProvider={dataProvider}
+                            liveProvider={liveProvider}
+                            notificationProvider={useNotificationProvider}
+                            routerProvider={routerBindings}
+                            authProvider={authProvider}
+                            options={{
+                                syncWithLocation: true,
+                                warnWhenUnsavedChanges: true,
+                                useNewQueryKeys: true,
+                                projectId: "ZmE05r-dxIvNx-6A4YGY",
+                                liveMode: "auto",
+                            }}>
+                            <Routes>
+                                <Route path="/register" element={<Register />} />
+                                <Route path="/login" element={<Login />} />
+                                <Route path="/forgot-password" element={<ForgotPassword />} />
+                                <Route
+                                    element={
+                                        <Authenticated
+                                            key="authenticated-layout"
+                                            fallback={<CatchAllNavigate to="/login" />}>
+                                            <Layout>
+                                                <Outlet />
+                                            </Layout>
+                                        </Authenticated>
+                                    }>
+                                    <Route index element={<Home />} />
+                                </Route>
+                            </Routes>
+                            <RefineKbar />
+                            <UnsavedChangesNotifier />
+                            <DocumentTitleHandler />
+                        </Refine>
+                        <DevtoolsPanel />
+                    </DevtoolsProvider>
+                </AntdApp>
+            </RefineKbarProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,14 @@
 import { AuthPage } from "@refinedev/antd";
+import { authCredentials } from "../../providers/auth";
 
 export const Login = () => {
-  return (
-    <AuthPage
-      type="login"
-      formProps={{
-        initialValues: { email: "demo@refine.dev", password: "demodemo" },
-      }}
-    />
-  );
+    return (
+        <AuthPage
+            type="login"
+            formProps={{
+                // /data/index,ts
+                initialValues: authCredentials,
+            }}
+        />
+    );
 };
