a1b19b439d5b8755da2f16ce474d9952077abaa2
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2024-04-01 15:25:40 +0800

    added upcoming event
    - added styling
    - added useList
    - added skeleton loading

---
+++ src/components/index.ts
@@ -0,0 +1,16 @@
+import UpcomingEvents from "./home/upcoming-event";
+import DealsChart from "./home/deals-chart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events-skeleton";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+export {
+    UpcomingEvents,
+    DealsChart,
+    UpcomingEventsSkeleton,
+    AccordionHeaderSkeleton,
+    KanbanColumnSkeleton,
+    ProjectCardSkeleton,
+    LatestActivitiesSkeleton,
+};
